0b0fc6cc9de5b0b1757ad85cd2ab174ac234e0cd
WDsayeed <sayeedahammed526@gmail.comm>
2023-04-16 13:01:03 +0600

    added sign

--- src/component/providers/AuthProvider.jsx
+++ src/component/providers/AuthProvider.jsx
@@ -1,5 +1,5 @@
-import React, { createContext, useState } from 'react';
-import { createUserWithEmailAndPassword, getAuth, signInWithEmailAndPassword } from "firebase/auth";
+import React, { createContext, useEffect, useState } from 'react';
+import { createUserWithEmailAndPassword, getAuth, onAuthStateChanged, signInWithEmailAndPassword, signOut } from "firebase/auth";
 import app from '../../firebase/firebase.config';
 
 
@@ -15,11 +15,25 @@ const createUser = (email, password)=>{
 const signIn = (email, password)=>{
         return signInWithEmailAndPassword(auth, email, password)
 }
+const logOut = ()=>{
+       return signOut(auth)
+}
+
+useEffect( ()=>{
+        const unsubscribe = onAuthStateChanged(auth, currentUser=>{
+                console.log('auth state change', currentUser)
+                setUser(currentUser)
+        })
+        return ()=>{
+                unsubscribe()
+        }
+},[])
 
 const AuthInfo ={
         user,
         createUser,
-        signIn
+        signIn,
+        logOut
 }
 
         return (
